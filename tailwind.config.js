a71c98bc8e762071574c890cc0d6d2912cc771f9
Zahra Rostami <zahrarostami137878@gmail.com>
2025-11-24 21:27:07 +0330

    04-09-03-Fix tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
