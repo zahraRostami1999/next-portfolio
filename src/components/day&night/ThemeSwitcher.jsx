25c676f1d0d8833562513884b48c62bf79669d7c
Zahra Rostami <zahrarostami137878@gmail.com>
2025-11-24 20:08:42 +0330

    04-09-03-save theme

--- src/components/day&night/ThemeSwitcher.jsx
+++ src/components/day&night/ThemeSwitcher.jsx
@@ -4,9 +4,18 @@ import { HiOutlineSun, HiOutlineMoon } from 'react-icons/hi';
 const ThemeSwitcher = () => {
   const [isDark, setIsDark] = useState(false);
 
+  const handleChangeTheme = () => {
+    setIsDark(!isDark);
+    if (isDark) {
+      localStorage.setItem("theme", "dark")
+    }else if(!isDark){
+      localStorage.setItem("theme", "light")
+    }
+  }
+
   return (
     <button
-      onClick={() => setIsDark(!isDark)}
+      onClick={() => handleChangeTheme()}
       className="mx-auto p-1.5 rounded-lg transition-colors duration-200 hover:bg-gray-100"
       aria-label="تغییر تم"
     >
