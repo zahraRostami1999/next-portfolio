47eb6ccb4a9871d2c7f447167b30b4fd46fed993
Zahra Rostami <zahrarostami137878@gmail.com>
2025-11-29 12:23:40 +0330

    04-09-08-Update bg color

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="px-10 bg-rose-200 dark:bg[var(--bg-primary))]">
+    <div className="px-10 bg-(--bg-primary)">
       <Component {...pageProps} />
     </div>
   )
