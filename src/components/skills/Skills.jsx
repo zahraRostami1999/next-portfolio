d9793e5cbb7b8c8f38ca34de8742a01f395b07a1
Zahra Rostami <zahrarostami137878@gmail.com>
2025-12-16 20:49:50 +0330

    04-09-25-Edit Skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,8 +5,8 @@ import SkillCard from '../card/SkillCard';
 export const Skills = () => {
   return (
     <div className="bg-(--bg-container) backdrop-blur-sm rounded-2xl p-8 border border-(--border)">
-      <h2 className="text-3xl font-semibold text-center mb-10 text-(--text-main)">
-        My<span className="text-(--text-secondary)"> Skills</span>
+      <h2 className="text-4xl font-extrabold md:text-5xl text-center mb-20 text-(--text-secondary)">
+        Skills
       </h2>
 
       <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-4 gap-10 max-w-5xl mx-auto">
@@ -16,15 +16,15 @@ export const Skills = () => {
             initial={{ opacity: 0, y: 40 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
-            transition={{ 
-              duration: 0.6, 
-              delay: index * 0.3   
+            transition={{
+              duration: 0.6,
+              delay: index * 0.3
             }}
           >
-            <SkillCard 
-              name={skill.name} 
-              icon={skill.icon} 
-              bgColor={skill.bg} 
+            <SkillCard
+              name={skill.name}
+              icon={skill.icon}
+              bgColor={skill.bg}
             />
           </motion.div>
         ))}
